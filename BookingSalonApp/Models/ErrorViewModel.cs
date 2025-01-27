397e1c6a0da7cad1c66f90bfd211ed1c7997fcbd
ivan0301 <ivangrgic0301@gmail.com>
2025-01-27 18:13:07 +0100

    Add project files.

---
+++ BookingSalonApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingSalonApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
